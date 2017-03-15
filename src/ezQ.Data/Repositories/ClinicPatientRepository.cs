5d9e96110ad05c324c9724dc3d0255e415a5ed39
bob1457 <bob.yuan@yahoo.com>
2017-03-14 21:19:08 -0700

    Implement clinic controller and test

--- src/ezQ.Data/Repositories/ClinicPatientRepository.cs
+++ src/ezQ.Data/Repositories/ClinicPatientRepository.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ezQ.Core.Entities;
 using ezQ.Services.Interfaces;
 using ezQ.Services.ViewModels;
-using ezQ.Data.Interfaces;
 
 namespace ezQ.Data.Repositories
 {
